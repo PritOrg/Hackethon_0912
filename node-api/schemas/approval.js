4bc085a3aa6d38885a33d6304fd1ef30f93e356f
Bhargav-1001 <bhargavbhagiya10@gmail.com>
2023-12-09 09:40:34 +0530

    router balance appoval

---
+++ node-api/schemas/approval.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Approval = new mongoose.Schema({
+    id: String,
+    leaveRequestId: String,
+    approverId: String, // User ID of the approver
+    status: String, // Approved, Rejected, Pending, etc.
+    comments: String,
+    createdAt: Date,
+  });
+module.exports = new mongoose.model('Approval', Approval);
+  
